321b6819b6d2a52254c8d71cd6ca73ea3861b8fd
Ibrahim <153656440+supibby@users.noreply.github.com>
2024-10-06 00:47:52 +0100

    define types needed

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,1 +1,10 @@
 // types.ts
+
+import { ReactNode, ButtonHTMLAttributes } from 'react'
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode
+  variant?: 'primary' | 'secondary' | 'primary-outline' | 'secondary-outline'
+  icon?: ReactNode
+  loading?: boolean
+}
