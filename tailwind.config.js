582407ba2c1dc9b3c15beafdde282387c6cc13b2
Ibrahim <153656440+supibby@users.noreply.github.com>
2024-10-06 17:25:54 +0100

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './stories/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
