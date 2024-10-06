931472bd280e0ad6211827896b7df07d34b17a59
Ibrahim <153656440+supibby@users.noreply.github.com>
2024-10-06 17:29:09 +0100

    create first test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,1 +1,10 @@
 // Button.test.tsx
+import { render, screen } from '@testing-library/react'
+import Button from './Button'
+
+describe('Button', () => {
+  test('renders button with correct text', () => {
+    render(<Button>Click me</Button>)
+    expect(screen.getByText('Click me')).toBeInTheDocument()
+  })
+})
