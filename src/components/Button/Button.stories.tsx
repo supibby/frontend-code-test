4d9379f564a4927ae970d0daff9bdbeb884cd41f
Ibrahim <153656440+supibby@users.noreply.github.com>
2024-10-06 17:28:18 +0100

    create stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,1 +1,56 @@
-// Button.stories.tsx
+import { Meta, StoryObj } from '@storybook/react'
+import Button from './Button'
+import { ComponentProps } from 'react'
+
+type StoryProps = ComponentProps<typeof Button>
+
+const meta: Meta<StoryProps> = {
+  component: Button,
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+        options: [
+          'primary',
+          'secondary',
+          'primary-outline',
+          'secondary-outline',
+        ],
+      },
+    },
+    icon: { control: 'boolean' },
+    loading: { control: 'boolean' },
+    disabled: { control: 'boolean' },
+  },
+  args: {
+    children: 'Button',
+  },
+}
+
+export default meta
+
+type Story = StoryObj<StoryProps>
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+  },
+}
+
+export const WithIcon: Story = {
+  args: {
+    icon: true,
+  },
+}
+
+export const Loading: Story = {
+  args: {
+    loading: true,
+  },
+}
