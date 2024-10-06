1533b53fc86c47bab3e266c5b5d1b493c5233f1f
Ibrahim <153656440+supibby@users.noreply.github.com>
2024-10-06 21:17:52 +0100

    create the 3 tests

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 import Button from './Button'
 import { ComponentProps } from 'react'
 import { CheckIcon } from '../../assets/icons/checkIcon'
@@ -42,6 +43,10 @@ type Story = StoryObj<StoryProps>
 export const Primary: Story = {
   args: {
     variant: 'primary',
+    onClick: () => {
+      console.log('hi')
+      action('button-click')('Custom click handler: hi')
+    },
   },
 }
 
--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,10 +1,48 @@
 // Button.test.tsx
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Button from './Button'
 
+const MockIcon = () => <svg data-testid="mock-icon" />
+
 describe('Button', () => {
-  test('renders button with correct text', () => {
-    render(<Button>Click me</Button>)
-    expect(screen.getByText('Click me')).toBeInTheDocument()
+  test('button is disabled and does not trigger onClick when disabled prop is true', () => {
+    const onClickMock = jest.fn()
+    const { getByRole } = render(
+      <Button disabled onClick={onClickMock}>
+        Disabled Button
+      </Button>
+    )
+
+    const buttonElement = getByRole('button', { name: 'Disabled Button' })
+
+    expect(buttonElement).toBeDisabled()
+
+    fireEvent.click(buttonElement)
+
+    expect(onClickMock).not.toHaveBeenCalled()
+  })
+
+  test('button is correctly in a loading state', () => {
+    const { container } = render(<Button loading>Loading</Button>)
+
+    const buttonElement = container.querySelector('button')
+
+    expect(buttonElement).toBeDisabled()
+    expect(buttonElement).toHaveClass('button--loading')
+  })
+
+  test('renders button with icon correctly', () => {
+    const { getByTestId, getByRole, getByText } = render(
+      <Button icon={MockIcon}>Button with Icon</Button>
+    )
+
+    const iconElement = getByTestId('mock-icon')
+    expect(iconElement).toBeInTheDocument()
+
+    const buttonElement = getByRole('button')
+    expect(buttonElement).toHaveClass('button--with-icon')
+
+    const textElement = getByText('Button with Icon')
+    expect(textElement).toBeInTheDocument()
   })
 })
