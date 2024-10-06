c1b87243e6b7aa130d5b56f43479d72467a95647
Ibrahim <153656440+supibby@users.noreply.github.com>
2024-10-06 22:23:45 +0100

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'lounge-off-white--light': '#F0F0F0',
         'lounge-off-white': '#E5E5E5',
         'lounge-grey--light': '#E7E7E7',
         'loung-grey--mid': '#C8C8C8',
         'lounge-grey': '#929292',
         'lounge-grey--dark': '#7D7D7D',
+        'lounge-grey--darker': '#757272',
       },
     },
   },
